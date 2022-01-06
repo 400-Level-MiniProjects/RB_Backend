db5ebf0000fc967b39724aa020ee69824240fbc6
milinches <ibrogenius5165@gmail.com>
2022-01-06 14:04:33 +0100

    Created user model 👻

--- rb_backend/result_broadsheet/admin.py
+++ rb_backend/result_broadsheet/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import *
+
+# Register AdminModels here
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('email', 'first_name', 'last_name', 'date_joined')
+    search_fields = ('email', 'first_name', 'last_name',)
 
 # Register your models here.
+admin.site.register(User, UserAdmin)
